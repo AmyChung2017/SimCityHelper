2bfc6d5460a67f21ef4bd78625464eaf2f9c8c4a
AmyChung2017 <amychung201703@gmail.com>
2018-11-03 11:45:57 +0800

    更新

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -12,16 +12,16 @@ namespace DAL
     public class Test
     {
         //数据库连接字符串（数据库地址、数据库名、数据库用户名、数据库密码）
-        private static string ConnString = "Host=localhost;Database=xxxx;Port=xxxx;UserId=xxxx;Password=xxxx";
+        private static string ConnString = "Host=localhost;Database=simcity;Port=3306;UserId=root;Password=12345678";
 
         public Test() { }
 
         public DataTable GetSomeData() {
-            string commandText = "SELECT * FROM xxx.xxx";
+            string commandText = "SELECT * FROM simcity.materials";
             DataSet dt = SqlHelper.ExecuteDataset(ConnString, CommandType.Text, commandText);
-            DataTable customerTable = dt.DefaultViewManager.DataSet.Tables[0];
+            DataTable customerTable = dt.Tables[0];
             //DataTable customerTable = dt.Tables["materials"];
-            
+
             //???
             customerTable.Locale = System.Globalization.CultureInfo.InvariantCulture;
 
